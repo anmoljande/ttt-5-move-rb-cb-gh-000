024f7c6fa8ffe87ed06d3347a2abb0dd94bd7828
Anmol Jande <jandanmol@gmail.com>
2019-01-01 08:57:48 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(num)
+
+  
+
+
+end
+
+def move(board,pindex,val="X")
+  
+
+end
+
+
 # code your input_to_index and move method here!
